e72d4de8e7e53efcdb92b5a0c8a3b027569411cf
Trevor Keith <tk@tkeith.com>
2021-12-26 18:13:36 -0800

    mongo setting/getting working

---
+++ notes.md
@@ -0,0 +1,3 @@
+# cli import
+
+`import('./db.js').then(m => m.getDb()).then(db => db.collection('test').findOne()).then(row => console.log(row))`
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,28 @@
 import { Router } from "express"
+import { getDb } from "./db.js"
+import { ObjectId } from "mongodb"
 
-const routes = Router()
+export const routes = Router()
 
-routes.route('/').get(function (req, res) {
-  res.json({ hello: 'world' })
+routes.route('/').get((req, res) => {
+  getDb()
+    .then((db) => {
+      db.collection('test').findOne({}).then(row => res.json({ text: row?.text || "no data yet" }))
+    })
+    .catch((err) => { console.log(err) })
+})
+
+routes.route('/save').post((req, res) => {
+  getDb()
+    .then((db) => {
+      db.collection('test').updateOne(
+        {},
+        { $set: { text: req.body.text } },
+        {
+          upsert: true,
+        }).then(() =>
+          res.json())
+    })
 })
 
 export default routes
