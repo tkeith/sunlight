9b54748439695fe6dd3704e6ce12df047222d5b5
Trevor Keith <tk@tkeith.com>
2022-01-28 19:21:52 -0700

    basic dapp

--- src/next/pages/_app.js
+++ src/next/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { Ropsten, DAppProvider, useEtherBalance, useEthers, Config } from '@usedapp/core'
+import { formatEther } from '@ethersproject/units'
+
+const dappConfig = {
+  readOnlyChainId: Ropsten.chainId,
+  readOnlyUrls: {
+    [Ropsten.chainId]: 'https://ropsten.infura.io/v3/51eeb67768ac4350add3fa2acd66fa67',
+  },
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <DAppProvider config={dappConfig}>
+    <Component {...pageProps} />
+  </DAppProvider>
 }
 
 export default MyApp
