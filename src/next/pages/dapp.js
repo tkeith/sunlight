c812078563f71b58d18c1edbd245e872c04586e5
Trevor Keith <tk@tkeith.com>
2022-03-23 15:27:45 -0400

    basic dapp update & gitignore cypress videos

--- src/next/pages/dapp.js
+++ src/next/pages/dapp.js
@@ -5,28 +5,28 @@ export default function dappPage() {
   const { activateBrowserWallet, account } = useEthers()
   const etherBalance = useEtherBalance(account)
   return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => activateBrowserWallet()} className="
-            px-6
-            py-2.5
-            bg-blue-600
-            text-white
-            font-medium
-            text-xs
-            leading-tight
-            uppercase
-            rounded
-            shadow-md
-            hover:bg-blue-700 hover:shadow-lg
-            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg
-            transition
-            duration-150
-            ease-in-out">Connect</button>
-        </div>
-        {account && <p>Account: {account}</p>}
-        {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+        <button onClick={() => activateBrowserWallet()} className="
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg
+          transition
+          duration-150
+          ease-in-out">Connect</button>
       </div>
+      {account && <p>Account: {account}</p>}
+      {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+    </div>
   )
 }
