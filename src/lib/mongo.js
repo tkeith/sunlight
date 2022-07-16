ccf5a6d727f4a7726fa138517ca535bbc094f154
Trevor Keith <tk@tkeith.com>
2022-07-16 14:54:36 -0400

    fix retry logic for mongo & redis

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb"
+import asyncRetryTimeout from "./asyncRetryTimeout.js"
 import callOnce from "./callOnce.js"
 
-const getMongo = callOnce(async () => {
-  try {
-    const conn = await MongoClient.connect('mongodb://app:app@mongo:27017/')
-    return conn.db("app")
-  } catch (err) {
-    await new Promise(r => setTimeout(r, 1000))
-    return await connect()
-  }
-});
+const connect = async () => {
+  const conn = await MongoClient.connect('mongodb://app:app@mongo:27017/')
+  return conn.db("app")
+}
+
+const retryer = async () => asyncRetryTimeout(60000, 1000, connect);
+
+const getMongo = callOnce(retryer);
 
 export default getMongo
--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,17 +1,16 @@
 import { createClient } from 'redis';
 import callOnce from './callOnce.js';
 
-const getRedis = callOnce(async () => {
+const connect = async () => {
   const client = createClient({
     url: 'redis://redis:6379'
   })
-  try {
-    await client.connect()
-    return client
-  } catch (err) {
-    await new Promise(r => setTimeout(r, 1000))
-    return await connect()
-  }
-});
+  await client.connect()
+  return client
+}
+
+const retryer = async () => asyncRetryTimeout(60000, 1000, connect);
+
+const getRedis = callOnce(retryer);
 
 export default getRedis
