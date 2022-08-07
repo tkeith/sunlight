8bacac23aeaae2df3dac0f95859857760914490b
Trevor Keith <tk@tkeith.com>
2022-08-07 17:03:01 -0400

    support multiple contracts

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,29 +1,37 @@
 import { readFileSync } from "fs"
 import hre, { ethers, upgrades } from "hardhat"
+import _ from "lodash"
 
-const CONTRACT_NAME = "MainContract"
+const contract_names = ["MainContract"]
 
 async function main() {
-  const Contract = await ethers.getContractFactory(CONTRACT_NAME)
-  
-  const contract = await upgrades.deployProxy(Contract, [], {initializer: 'initialize', kind: 'transparent'})
 
-  await contract.deployed()
+  let contractDeployments: any = {}
 
-  const networkName = (await contract.provider.getNetwork()).name
-  const address = contract.address
-  const abi = JSON.parse(readFileSync(`./artifacts/contracts/${CONTRACT_NAME}.sol/${CONTRACT_NAME}.json`, 'utf8')).abi
+  for (const contract_name of contract_names) {
+    const Contract = await ethers.getContractFactory(contract_name)
+
+    const contract = await upgrades.deployProxy(Contract, [], { initializer: 'initialize', kind: 'transparent' })
+
+    await contract.deployed()
+
+    const networkName = (await contract.provider.getNetwork()).name
+    const address = contract.address
+    const abi = JSON.parse(readFileSync(`./artifacts/contracts/${contract_name}.sol/${contract_name}.json`, 'utf8')).abi
+
+    _.merge(contractDeployments, {
+      [networkName]: {
+        [contract_name]: {
+          address: address,
+            abi: abi
+        }
+      }
+    })
+  }
 
   console.log('*** beginning of json result ***')
   console.log(JSON.stringify({
-    contract_deployments: {
-      [networkName]: {
-        [CONTRACT_NAME]: {
-          address: address,
-          abi: abi
-        }
-      }
-    }
+    contract_deployments: contractDeployments
   }))
   console.log('*** end of json result ***')
 }
